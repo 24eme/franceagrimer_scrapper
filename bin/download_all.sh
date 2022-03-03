40835b65dd2a84fff8390d49cff4a0cfefeb5e85
Tangui Morlier <tangui@tangui.eu.org>
2022-03-03 12:51:13 +0100

    Permet d'avoir deux instances en parallèle

--- bin/download_all.sh
+++ bin/download_all.sh
@@ -15,24 +15,24 @@ fi
 
 date=$(date +%Y%m%d%H%M)
 
-mkdir -p download export
-rm -f download/Export-*csv
+mkdir -p download/$$ export
+rm -f download/$$/Export-*csv
 
-node puppeteer_scripts/export_csv_campagne.js download $campagne
-if ! test -f download/Export-*csv ; then
+node puppeteer_scripts/export_csv_campagne.js download/$$ $campagne
+if ! test -f download/$$/Export-*csv ; then
 	echo "L'export a échoué pour la campagne $campagne"
 	exit 1
 fi
 csvdossier=export/$date"_"$campagne"_"dossiers.csv
-mv -f download/Export-*csv $csvdossier
+mv -f download/$$/Export-*csv $csvdossier
 recode ISO88591..UTF8 $csvdossier
-rm -f download/*pdf
+rm -f download/$$/*pdf
 cat $csvdossier | awk -F ';' '{print $3}' | grep '^[0-9]' | while read dossier ; do
-	node puppeteer_scripts/export_pdf_campagne_cvi.js download $campagne $dossier
-	if ! test -f download/*pdf ; then
+	node puppeteer_scripts/export_pdf_campagne_cvi.js download/$$ $campagne $dossier
+	if ! test -f download/$$/*pdf ; then
 		echo "ERREUR: $dossier non récupéré";
 	else
-		mv -f download/*pdf export/
+		mv -f download/$$/*pdf export/
 	fi
 done
 
