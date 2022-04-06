6ea68cb04c1b2925c7aecaba44781edc1b0eba24
Tangui Morlier <tangui@tangui.eu.org>
2022-04-06 19:57:38 +0200

    Parsing d'un PDF

--- node_scripts/scrap_xml_pdf_dossier.js
+++ node_scripts/scrap_xml_pdf_dossier.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var debug = 0;
 
 try {
     // read contents of the file
@@ -9,9 +10,9 @@ try {
     // split the contents by new line
     const lines = data.split(/\r?\n/);
 
-    var page = 'page garde';
     var i = 0;
     var parcelleid = 0;
+    var cadastraleid = 0;
     var oldtop = 0;
     var action = '';
     var oldaction = 'OLD';
@@ -22,16 +23,25 @@ try {
     lines.forEach((line) => {
         let top = line.split('"')[1]
         let left = line.split('"')[3]
+        if (line.match(/<image .*jpg/)) {
+            parcelleid++;
+        }
         line = line.replace(/<[^>]*>/g, ';');
         line = line.replace(/^;?[ \t]*/, '');
         line = line.replace(/[ \t]*;?$/, '');
-        i++;
-        //console.log("DEBUG: line ("+i+") "+line);
-        if (line.match(';Dossier N°')) {
-            action = '';
-            return;
+        line = line.replace(':',';');
+        if (line.match(/^Statut parcelle/) || line.match(/^Date transmission/)) {
+            line = ';'+line;
         }
+        i++;
+        if (debug) console.log("DEBUG: line ("+i+") "+line);
         switch (line.split(';')[1]) {
+            case 'Dossier N° ':
+                if (action == 'parcelles restructuration') {
+                    parcelleid = 0;
+                }
+                action = '';
+                return;
             case 'N° CVI':
                 buffervalue = '';
                 action = 'CVI';
@@ -74,6 +84,9 @@ try {
                 action = 'nextline';
                 return ;
             case 'N°':
+                if (action == 'controles realises droits') {
+                    return;
+                }
                 key = 'Engagement n°';
                 action = 'nextline';
                 return ;
@@ -96,6 +109,9 @@ try {
                 action = 'getline4';
                 return ;
             case 'densité':
+                if (action == 'controles realises droits') {
+                    return;
+                }
                 key = 'Option PCR retenue pour le recours au changement de densité';
                 action = 'nextline';
                 return ;
@@ -113,9 +129,50 @@ try {
             case 'Parcelles Restructuration':
                 action = 'parcelles restructuration';
                 return ;
+            case 'Parcelle demandée ':
+            case 'Surface demandée ':
+            case 'Action ':
+            case 'Action complémentaire ':
+            case 'Objectifs principaux ':
+            case 'Commune ':
+            case 'Section ':
+            case 'EIR ':
+            case 'EIP ':
+            case 'Cépage ':
+            case 'Statut parcelle ':
+            case 'Date transmission ST ':
+            case 'Appellation ':
+            case 'Densité (pieds/ha) ':
+                key = 'Détail des parcelles '+parcelleid+' '+line.split(';')[1].trim();
+                action = 'sameline';
+                if(line == ";Statut parcelle ; En cours d’;") {
+                    line = ";Statut parcelle ; En cours d’instruction";
+                }
+                break;
+            case 'Parcelles plantées qui composent la parcelle culturale':
+                action = 'parcelle culturale';
+                cadastraleid = 1;
+                break;
+            case 'Règles supplémentaires':
+                key = 'Détail des parcelles '+parcelleid+' Règles supplémentaires'
+                action = 'nextline';
+                return;
+            case 'Contrôles réalisés':
+                if (action == 'sameline') {
+                    action = 'controles realises';
+                }else{
+                    action = 'controles realises droits';
+                }
+                return;
+            case 'Surface retenue suite ':
+                action = '';
+                return;
+            case 'Liste des droits / des arrachages / des parcelles à arracher':
+                action = 'liste des droits';
+                return;
             default:
         }
-        console.log("DEBUG: action: "+action+" != "+oldaction);
+        if (debug) console.log("DEBUG: action: "+action+" != "+oldaction);
         if (action != oldaction) {
             i = 0;
         }
@@ -164,11 +221,11 @@ try {
                 oldaction = '';                
                 break;
             case 'Total surfaces demandées par action':
-                if (i == 0) key = 'PLA-indiv';
-                if (i == 1) key = 'PLA-coll';
-                if (i == 2) key = 'PAL-seul';
-                if (i == 3) key = 'IRR-seule';
-                if (i == 4) key = 'PAL+IRR-seuls';                
+                if (i == 0) key = 'Total surfaces PLA-indiv';
+                if (i == 1) key = 'Total surfaces PLA-coll';
+                if (i == 2) key = 'Total surfaces PAL-seul';
+                if (i == 3) key = 'Total surfaces IRR-seule';
+                if (i == 4) key = 'Total surfaces PAL+IRR-seuls';
                 break;
             case 'RIB':
                 if (i < 3 || i > 6) {
@@ -189,7 +246,7 @@ try {
                     parcelleid++;
                 }
                 oldtop = top;
-                key = 'parcelle '+parcelleid;
+                key = 'Résumé parcelle '+parcelleid;
                 if (left > 1100) {
                     key += ' Objectifs principaux';
                 }else if (left > 1000) {
@@ -214,12 +271,119 @@ try {
                     key += ' N° GPS/ Surf. retenue'
                 }
                 break;
+            case 'parcelle culturale':
+                if (i < 13) {
+                    buffervalue = '';
+                    return;
+                }
+                if (buffervalue == 'Total ') {
+                    key = '';
+                    action = '';
+                    return;
+                }
+                key = 'Détail des parcelles '+parcelleid+' Parcelle culturale '+cadastraleid;
+                if (left > 420) {
+                    cadastraleid++;
+                    key += " Surface demandée"
+                } else if (left > 320) {
+                    key += " Campagne de plantation"
+                } else if (left > 190) {
+                    key += " Surface au CVI"
+                }else if (left > 120) {
+                    key += " Numéro d'ordre"
+                } else if (left > 25) {
+                    key += ' Référence cadastrale'
+                } else if (left > 20) {
+                    key = 'Détail des parcelles '+parcelleid+' Parcelle culturale id';
+                }
+                break;
+            case 'controles realises':
+                if (i % 2 == 0) {
+                    key = 'Détail des parcelles '+parcelleid+' Contrôles réalisés '+buffervalue.trim();
+                    buffervalue = '';
+                    return;
+                }
+                break;
+            case 'liste des droits':
+                if (i < 25 || (i < 31 && left > 500)) {
+                    buffervalue = '';
+                    return;
+                }
+                key = 'Détail des parcelles '+parcelleid+' Liste des droits '+cadastraleid+' ';
+                if (left > 1300) {
+                    key += "Code mesure RS"; 
+                } else if (left > 1200) {
+                    key += "Dossier AP (hors PCL)";
+                } else if (left > 1060) {
+                    key += "Résultat instruction";
+                } else if (left > 990) {
+                    key += "Type de instruction";
+                } else if (left > 850) {
+                    key += "Surface demandée";
+                } else if (left > 700) {
+                    key += "Cépage";
+                } else if (left > 610) {
+                    key += "Densité (pieds/ha)";
+                } else if (left > 570) {
+                    key += "EIP";
+                } else if (left > 530) {
+                    key += "EIR";
+                } else if (left > 400) {
+                    key += "Appellation";
+                } else if (left > 260) {
+                    key += "Date d'arrachage";
+                } else if (left > 145) {
+                    key += 'Référence cadastrale';
+                } else if (left > 110) {
+                    key += 'Type de droit';
+                } else if (left > 60) {
+                    key += 'N° du droit';
+                } else if (left > 30) {
+                    cadastraleid = buffervalue.trim();
+                    buffervalue = '';
+                    return;
+                } else if (left > 20) {
+                    key = 'Détail des parcelles '+parcelleid+' Liste des droits id';
+                }
+                break;
+            case 'controles realises droits':
+                if (i < 15 || (i < 20 && left > 300)) {
+                    buffervalue = '';
+                    return;
+                }
+                if (buffervalue.match("Date d'impression")) {
+                    action = '';
+                    return ;
+                }
+                key = 'Détail des parcelles '+parcelleid+' Liste des droits '+cadastraleid+' ';
+                if (left > 650) {
+                    key += 'Résultat instruction automatique'
+                } else if (left > 550) {
+                    key += 'Règle supplémentaire'
+                } else if (left > 450) {
+                    key += 'Calcul densité'
+                } else if (left > 400) {
+                    key += 'Débrayage RMD'
+                } else if (left > 300) {
+                    key += 'Cible densité'
+                } else if (left > 200) {
+                    key += 'Croisement de cépage'
+                } else if (left > 130) {
+                    key += 'Constrôle de restructuration'
+                } else if (left > 60) {
+                    key += 'Type de restructuration'
+                } else if (left > 30) {
+                    cadastraleid = buffervalue.trim();
+                    buffervalue = '';
+                    return;
+                }
+                break;
             default:
                 if (line) {
-                    console.log("DEBUG: line ("+i+" t:"+top+" l:"+left+") : "+line+" < "+action);
+                    if (debug) console.log("DEBUG: line ("+i+" t:"+top+" l:"+left+") : "+line+" < "+action);
                 }
                 buffervalue = '';
-                return 
+                return;
         }
         if (key && buffervalue) {
             console.log(key+';'+buffervalue.trim());
