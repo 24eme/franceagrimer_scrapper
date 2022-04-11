6f56f862db700c40cd0e35e6d9e00510b250717c
Tangui Morlier <tangui@tangui.eu.org>
2022-04-11 15:51:55 +0200

    Conversion de tout les PDF

---
+++ bin/convert_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ls export/*pdf | while read file ; do
+	pdf=$(basename $file)
+     	xml=$(echo "$pdf" | sed 's/pdf$/xml/')
+        csv=$(echo "$pdf" | sed 's/pdf$/csv/')
+	mkdir -p tmp
+	cp $file tmp/
+	cd tmp/
+	pdftohtml -xml $pdf > /dev/null
+        cd ..
+	node node_scripts/scrap_xml_pdf_dossier.js "tmp/"$xml | sort -u > "res/"$csv
+#	rm -rf tmp
+done
