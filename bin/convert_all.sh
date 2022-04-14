8ee145eacc94cdddc2905833bdf9c39157b5ecb1
Tangui Morlier <tangui@tangui.eu.org>
2022-04-14 14:20:58 +0200

    Nom plus compréhensible

--- bin/convert_all.sh
+++ bin/convert_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 header="1"
-ls export/*pdf | while read file ; do
+ls livraison_20220413/*pdf | while read file ; do
 	pdf=$(basename $file)
      	xml=$(echo "$pdf" | sed 's/pdf$/xml/')
         csv=$(echo "$pdf" | sed 's/pdf$/csv/')
@@ -10,7 +10,7 @@ ls export/*pdf | while read file ; do
 	cd tmp/
 	pdftohtml -xml $pdf > /dev/null
         cd ..
-	node node_scripts/scrap_xml_pdf_dossier.js "tmp/"$xml | sort -u > "res/"$csv
+	node node_scripts/parsing_xml_pdf_dossier.js "tmp/"$xml | sort -u > "res/"$csv
 	node node_scripts/convert_csv_to_specific_format.js "res/"$csv $header
 	header="0"
 #	rm -rf tmp
