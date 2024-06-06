a37b9ee20d0f69496dd52ea90ca109855a472c73
Tangui Morlier <tangui@tangui.eu.org>
2024-06-06 12:49:29 +0200

    Permet de rejouer l'import d'une date précise

--- bin/download_all.sh
+++ bin/download_all.sh
@@ -7,32 +7,41 @@ export FRANCEAGRIMER_PASSWORD
 export FRANCEAGRIMER_DEBUG
 
 campagne=$1
-
+date=$2
 if ! test "$campagne"; then
 	echo "Campagne attendu";
 	exit 2
 fi
-
+if ! test "$date"; then
 date=$(date +%Y%m%d%H%M)
+fi
 
 mkdir -p download/$$ export
 rm -f download/$$/Export-*csv
+
+csvdossier=export/$date"_"$campagne"_"dossiers.csv
+
+if ! test -f "$csvdossier"; then
 
 node node_scripts/scrap_csv_campagne.js download/$$ $campagne
 if ! test -f download/$$/Export-*csv ; then
 	echo "L'export a échoué pour la campagne $campagne"
 	exit 1
 fi
-csvdossier=export/$date"_"$campagne"_"dossiers.csv
 mv -f download/$$/Export-*csv $csvdossier
 recode ISO88591..UTF8 $csvdossier
+
+fi
+
 rm -f download/$$/*pdf
-cat $csvdossier | awk -F ';' '{print $3}' | grep '^[0-9]' | while read dossier ; do
-	node node_scripts/scrap_pdf_campagne_cvi.js download/$$ $campagne $dossier
+cat $csvdossier | awk -F ';' '{print $3}' | grep '^[0-9]' | sed 's/DU$//' | while read dossier ; do
+	if ! find export/ -name "*"$dossier"*pdf" | grep "$dossier" ; then
+	node node_scripts/scrap_pdf_campagne_cvi.js download/$$ $campagne $dossier"DU"
 	if ! test -f download/$$/*pdf ; then
 		echo "ERREUR: $dossier non récupéré";
 	else
 		mv -f download/$$/*pdf export/
+	fi
 	fi
 done
 
